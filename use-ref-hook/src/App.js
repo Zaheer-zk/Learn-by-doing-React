361be8bcb58d6f5191ec06f859860b3c9fb74bfd
Zaheer khan <newzk16117541@gmail.com>
2023-03-14 23:13:17 +0530

    useRef code added

--- use-ref-hook/src/App.js
+++ use-ref-hook/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+
+//useRef help us render only when we done some changes
+// we also can get previous stage using useRef
 
 function App() {
+  const [input, setInput] = useState('');
+
+  // this useRef helping me to get the render state only when we do some changes
+  const counter = useRef(0);
+
+  const prevState = useRef('');
+
+  const inputField = useRef();
+
+  useEffect(() => {
+    counter.current = counter.current + 1;
+    prevState.current = input;
+  }, [input]);
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleButtonClick = () => {
+    console.log('inputField.current : ', inputField.current);
+    inputField.current.focus();
+    inputField.current.value = '';
+    inputField.current.style.border = '2px solid crimson';
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        ref={inputField}
+        type="text"
+        name="Input"
+        id="Input"
+        value={input}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleButtonClick}>Click me</button>
+      <h4>Application is rerendering this much time : {counter.current}</h4>
+      <h4>My prevState is {prevState.current}</h4>
+    </>
   );
 }
 
